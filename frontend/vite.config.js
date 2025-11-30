92acf1ff5a5b5c77bbcf9c0bafbd98f2a851d82c
Jongkuch1 <j.anyar@alustudent.com>
2025-11-30 10:37:18 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,15 @@
-"scripts": {
-  "dev": "vite",
-  "build": "vite build",
-  "start": "serve -s dist -l $PORT"
-}
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 3000,
+    strictPort: true
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 3000
+  }
+})
