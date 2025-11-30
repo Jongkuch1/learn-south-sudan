8affdcb98df88def85e283b3a41ca75252dd2e45
Jongkuch1 <j.anyar@alustudent.com>
2025-11-30 10:41:05 +0200

    Update Vite config to allow all hosts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
     port: process.env.PORT || 3000,
-    strictPort: true
-  },
-  preview: {
     host: '0.0.0.0',
-    port: process.env.PORT || 3000
+    allowedHosts: 'all'
   }
 })
